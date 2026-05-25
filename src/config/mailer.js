0789e13c26da000dd7e8e42c0af54dc5a667374b
Arpit-badiya <arpitpatidar2506@gmail.com>
2026-05-25 15:55:47 +0530

    update SMTP

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,7 +1,29 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// const sendOtp = async (email, otp) => {
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: "Your OTP",
+//     text: `Your OTP is ${otp}`,
+//   });
+// };
+
+// module.exports = sendOtp;
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
